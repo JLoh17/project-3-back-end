0b648455ba4106873a26a55bb2fe070202f6370a
JONATHAN LOH <loh_jonathan17@hotmail.COM>
2021-11-03 17:53:43 +0800

    uncommented auth routes

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,9 @@ const { Router } = require('express')
 const router = Router ()
 
 // Auth
-// router.post('/auth/signup', require('../controllers/api/auth/signup'))
-// router.post('/auth/login', require('../controllers/api/auth/login'))
-// router.delete('/auth/logout', require('../controllers/api/auth/logout'))
+router.post('/auth/signup', require('../controllers/api/auth/signup'))
+router.post('/auth/login', require('../controllers/api/auth/login'))
+router.delete('/auth/logout', require('../controllers/api/auth/logout'))
 
 // Public products
 router.get('/products'), require('../controllers/api/products') // no need to write index if already in the path
