78cb3e5c5a58d5c2bc19f81e4d874d6200862cc7
JONATHAN LOH <loh_jonathan17@hotmail.COM>
2021-11-11 17:06:09 +0800

    updated table to string and removed isInt for size validation

--- migrations/20211108071148-add-size-to-cart.js
+++ migrations/20211108071148-add-size-to-cart.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('Carts', 'size', {type: Sequelize.INTEGER });
+    await queryInterface.addColumn('Carts', 'size', {type: Sequelize.STRING });
   },
 
   down: async (queryInterface, Sequelize) => {
