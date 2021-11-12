97c123521dd83c080194d98381cbf51cb8e8aedf
gitjonathan <yootwodeetwo@gmail.com>
2021-11-12 10:43:53 +0800

    added seed

---
+++ seeders/20211112022458-data.js
@@ -0,0 +1,60 @@
+const { Faker } = require('fakergem')
+
+const { Category, Product } = require('../models')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const top = await Category.findOrCreate({ where: { catName: 'Men\'s Tops' } })
+    const shoe = await Category.findOrCreate({ where: { catName: 'Men\'s Shoes' } })
+    const hat = await Category.findOrCreate({ where: { catName: 'Men\'s Hats' } })
+
+    await Product.destroy({ truncate : true })
+    for (let i = 0; i < 10; i++) {
+      await Product.create({
+        productName: Faker.Commerce.productName(),
+        description: Faker.Lorem.sentence(3),
+        price: Faker.Number.between(100, 1000),
+        CategoryId: top.id,
+        Images: [
+          { imageURL: Faker.LoremFlickr.image('500x500', ['shirts', 'jacket'])},
+          { imageURL: Faker.LoremFlickr.image('500x500', ['shirts', 'jacket'])},
+          { imageURL: Faker.LoremFlickr.image('500x500', ['shirts', 'jacket'])}
+        ]
+      }, {
+        include: Product.Images
+      })
+    }
+
+    for (let i = 0; i < 10; i++) {
+      await Product.create({
+        productName: Faker.Commerce.productName(),
+        description: Faker.Lorem.sentence(3),
+        price: Faker.Number.between(100, 1000),
+        CategoryId: shoe.id,
+        Images: [
+          { imageURL: Faker.LoremFlickr.image('500x500', ['shoes'])},
+          { imageURL: Faker.LoremFlickr.image('500x500', ['shoes'])},
+          { imageURL: Faker.LoremFlickr.image('500x500', ['shoes'])}
+        ]
+      }, {
+        include: Product.Images
+      })
+    }
+
+    for (let i = 0; i < 10; i++) {
+      await Product.create({
+        productName: Faker.Commerce.productName(),
+        description: Faker.Lorem.sentence(3),
+        price: Faker.Number.between(100, 1000),
+        CategoryId: hat.id,
+        Images: [
+          { imageURL: Faker.LoremFlickr.image('500x500', ['hat', 'fedora'])},
+          { imageURL: Faker.LoremFlickr.image('500x500', ['hat', 'fedora'])},
+          { imageURL: Faker.LoremFlickr.image('500x500', ['hat', 'fedora'])}
+        ]
+      }, {
+        include: Product.Images
+      })
+    }
+  }
+}
